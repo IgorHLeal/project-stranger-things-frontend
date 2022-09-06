f2396212ff613fffeb496812a05af9c47abdfc28
Igor Leal <igorhenriqueleal3@gmail.com>
2022-09-06 19:40:15 -0300

    fix: Configuração nas variáveis de ambiente

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const development = process.env.REACT_APP_DEVELOPMENT === 'true';
-
 function App() {
   return (
     <div className="App">
-      { development && <h1>Em desenvolvimento</h1> }
+      { }
       <StrangerThings />
     </div>
   );
